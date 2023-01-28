c433651799b295fa156fb07e658cea7639832181
shivendra <shivendra0132@gmail.com>
2023-01-28 18:49:03 +0530

    object program added

---
+++ src/STringDublicateWordRelated/demo.java
@@ -0,0 +1,11 @@
+package STringDublicateWordRelated;
+
+import static java.lang.Character.*;
+
+public class demo {
+    public static void main(String[] args) {
+        String str ="12abc!";
+       int count=0;
+
+    }
+}
